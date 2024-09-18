bb784e5405201b9519597d6df11a8f752195ad4f
Pooja <poojasihag.dev@gmail.com>
2024-09-18 12:28:36 +0530

    i zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import List from "./components/list/List";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 const App = () => {
   const user = false;
 
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, []);
   return (
     <div className="container">
       {user ? (
@@ -19,7 +30,7 @@ const App = () => {
       ) : (
         <Login />
       )}
-      <Notification/>
+      <Notification />
     </div>
   );
 };
